a16a0732c199e6d4dce9579fd2548da4525134fb
33jumpstreet <jumpstreet0813@gmail.com>
2022-06-22 16:07:20 +0900

    header - 검색

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// 검색창 요소 찾기 
+const searchEl = document.querySelector(".search");
+const searchInputEl = document.querySelector(".search input");
+
+// 검색창 요소를 클릭하면 실행
+searchEl.addEventListener("click", function() {
+  searchInputEl.focus();
+});
+
+// 검색창 요소 내부 실제 input 요소에 포커스되면 실행
+searchInputEl.addEventListener('focus', function() {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+// 검색한 요소 내부 실제 input 요소에서 포커스가 해제(블러)되며 실행
+searchInputEl.addEventListener('blur', function() {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
