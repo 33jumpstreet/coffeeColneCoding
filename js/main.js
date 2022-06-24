d91585759cc1dbf902d9324795f17718546f086e
33jumpstreet <jumpstreet0813@gmail.com>
2022-06-24 17:37:12 +0900

    이미지 슬라이드 - 프로모션

--- js/main.js
+++ js/main.js
@@ -48,8 +48,39 @@ fadeEls.forEach(function(fadeEl, index) {
   });
 });
 
+// 공지사항 이미지 슬라이드 효과 - swiper
 new Swiper('.notice .swiper', {
   direction: 'vertical',
   autoplay : true,
   loop: true
+})
+
+// 프로모션 이미지 슬라이드 효과 - swiper
+new Swiper('.promotion .swiper', {
+  // direction : horizontal; // 수평 슬라이드
+  autoplay : true, // 자동 재생 여부
+  loop : true, // 반복 재생 여부
+  slidesPerView : 3, // 슬라이드 사이 여백
+  spaceBetween : 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드 가운데 보이기
+  pagination : { // 페이지 번호 사용
+    el : '.promotion .swiper-pagination', // 페이지 번호 요소
+    clickable : true // 사용자의 페이지 번호 제어 여부
+  },
+  navigation : { // 슬라이드 이전/ 다음 버튼 사용
+    prevEl: '.promotion .swiper-button-prev', // 이전 버튼 요소
+    nextEl: '.promotion .swiper-button-next' // 다음 버튼 요소
+  }
+})
+
+// 버튼을 눌렀을 때 프로모션이 열리고 닫힐 수 있는 기능
+const promotionEl = document.querySelector("section.promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+
+promotionToggleBtn.addEventListener("click", function() {
+  if (promotionEl.classList.contains('hide')) {
+    promotionEl.classList.remove('hide');
+  } else {
+    promotionEl.classList.add('hide');
+  }
 })
