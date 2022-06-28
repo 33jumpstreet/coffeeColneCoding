5ee51532d26f46760e2b7fd8a9491a345ff97182
33jumpstreet <jumpstreet0813@gmail.com>
2022-06-28 14:46:52 +0900

    푸터와 상단으로 이동 버튼 - 페이지 상단으로 이동

--- js/main.js
+++ js/main.js
@@ -21,6 +21,7 @@ searchInputEl.addEventListener('blur', function() {
 
 // 페이지 스크롤에 따른 요소 제어
 const badgeEl = document.querySelector("header .badges");
+const toTopEl = document.querySelector("#to-top");
 
 window.addEventListener("scroll", function() {
   if (window.scrollY > 500) {
@@ -29,13 +30,29 @@ window.addEventListener("scroll", function() {
       opacity : 0,
       display : 'none'
     });
+    // 상단으로 이동 버튼 보이기
+    gsap.to(toTopEl, .6, {
+      opacity: 1,
+      x : 0
+    });
   } else {
     // Badge 요소 보이기
     gsap.to(badgeEl, .6, {
       opacity : 1,
       display : 'block'
-    });    
+    });
+    // 상단으로 이동 버튼 숨기기
+    gsap.to(toTopEl, .6, {
+      opacity: 0,
+      x : 100
+    });   
   }
+});
+
+toTopEl.addEventListener('click', function() {
+  gsap.to(window, .6, {
+    scrollTo: 0
+  });
 });
 
 // 나타낼 요소(.fade-in)들을 찾기
